a29d893da47ca1a4a8525aa5ded887e0887a90ad
Felix <felix.chung@felixchung.net>
2024-05-29 18:25:38 +0800

    remove install

--- project/src/config.js
+++ project/src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    listDatasetUrl: "http://datamgmtdemo01.eastasia.cloudapp.azure.com/listalldatasets?db=admin"
-    ,descDatasetUrl: "http://datamgmtdemo01.eastasia.cloudapp.azure.com/showdatasetdesc?db=admin"
-    ,requestDatasetDeployUrl: "http://datamgmtdemo01.eastasia.cloudapp.azure.com/submit-ticket"
+    listDatasetUrl: "http://HOST_IP/listalldatasets?db=admin"
+    ,descDatasetUrl: "http://HOST_IP/showdatasetdesc?db=admin"
+    ,requestDatasetDeployUrl: "http://HOST_IP/submit-ticket"
 };
   
 export default config;

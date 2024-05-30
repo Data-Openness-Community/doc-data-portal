e401e506e11c40f3870de1a9f49723420c16e79f
Felix <felix.chung@felixchung.net>
2024-05-30 15:05:50 +0800

    Parametized genAiResponse

--- project/src/config.js
+++ project/src/config.js
@@ -2,6 +2,7 @@ const config = {
     listDatasetUrl: "http://HOST_IP/listalldatasets?db=admin"
     ,descDatasetUrl: "http://HOST_IP/showdatasetdesc?db=admin"
     ,requestDatasetDeployUrl: "http://HOST_IP/submit-ticket"
+    ,genAiResponseUrl: "http://HOST_IP/genai-response"
 };
   
 export default config;
